b95e312e80b689f56c57a8732d459ebc6eedd711
isubfox <samuel.liska@gmail.com>
2025-10-27 20:25:18 +0100

    addDir and Pipes

---
+++ dayPlannerApp/src/app/day-planner/day-planner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-planner',
+  imports: [],
+  templateUrl: './day-planner.html',
+  styleUrl: './day-planner.css',
+})
+export class DayPlanner {
+
+}
---
+++ dayPlannerApp/src/app/task-date-input/task-date-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-date-input',
+  imports: [],
+  templateUrl: './task-date-input.html',
+  styleUrl: './task-date-input.css',
+})
+export class TaskDateInput {
+
+}
